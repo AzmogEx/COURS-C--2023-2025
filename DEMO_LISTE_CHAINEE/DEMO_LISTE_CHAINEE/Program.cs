a9cd6374888c3f1158ead951073fb7442e850b0f
Adam M <80716388+AzmogEx@users.noreply.github.com>
2025-05-10 19:59:09 +0200

    Upload files from devops

---
+++ DEMO_LISTE_CHAINEE/DEMO_LISTE_CHAINEE/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+
+namespace DEMO_LISTE_CHAINEE;
+
+class C_LISTE_CHAINEE {
+    class C_MAILLON {
+        public int Data;
+        public C_MAILLON Suivant = null;
+    }
+    C_MAILLON Debut = null;
+
+    public void Rajoute_Info(int P_Data) {
+
+        C_MAILLON Nouveau = new C_MAILLON() { Data = P_Data };
+        Nouveau.Suivant = Debut;
+        Debut = Nouveau;
+
+    }
+
+    public void Affiche_Liste_Iter() {
+        var Courant = Debut;
+
+        while(Courant != null) {
+            Console.WriteLine(Courant.Data);
+            Courant = Courant.Suivant;
+        }
+    }
+
+    public void Affiche_Liste_Maillons() {
+        Affiche_Liste_Recursif(Debut);
+    }
+
+    void Affiche_Liste_Recursif(C_MAILLON P_Maillon) {
+        if(P_Maillon != null) {
+            Console.WriteLine(P_Maillon.Data);
+            Affiche_Liste_Recursif(P_Maillon.Suivant);
+        }
+    }
+}
+
+class Program {
+
+
+    //=============================================
+    static void Main(string[] args) {
+        //===========V1========================================
+        //Debut = new C_Maillon() { Data = 10,Suivant = null };
+        //C_Maillon Nouveau = new C_Maillon() { Data = 20,Suivant = null };
+        //Debut.Suivant = Nouveau;
+        //Nouveau = new C_Maillon() { Data = 30,Suivant = null };
+        //Debut.Suivant.Suivant = Nouveau;
+
+        C_LISTE_CHAINEE Ma_liste = new C_LISTE_CHAINEE();
+        Ma_liste.Rajoute_Info(10);
+        Ma_liste.Rajoute_Info(20);
+        Ma_liste.Rajoute_Info(30);
+        Ma_liste.Rajoute_Info(87);
+        Ma_liste.Rajoute_Info(23);
+        Ma_liste.Rajoute_Info(564);
+        Ma_liste.Rajoute_Info(3098);
+
+        Ma_liste.Affiche_Liste_Iter();
+        Console.WriteLine("-----------");
+        Ma_liste.Affiche_Liste_Maillons();
+    }
+}
---
+++ DEMO_REPERTOIRE/DEMO_REPERTOIRE/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace DEMO_REPERTOIRE; 
+internal class Program {
+
+   //static void Affiche_Info_Repertoire_V1() {
+   //     Console.WriteLine("Les fichiers:");
+   //     Console.WriteLine();
+   //     var Reponse_Fichier = Repertoire_Cible.EnumerateFiles();
+   //     foreach(var La_Reponse in Reponse_Fichier) {
+   //         Console.WriteLine(La_Reponse.Name);
+   //     }
+   //     Console.WriteLine("-----------------------");
+
+   //     Console.WriteLine("Les dossiers:");
+   //     Console.WriteLine();
+   //     var Reponse_Dossier = Repertoire_Cible.EnumerateDirectories();
+   //     foreach(var La_Reponse in Reponse_Dossier) {
+   //         Console.WriteLine(La_Reponse.Name);
+   //     }
+   // }
+
+    static void Affiche_Info_Repertoire_V2(string P_Chemin_Racine) {
+        DirectoryInfo Racine = new DirectoryInfo(P_Chemin_Racine);
+
+        var Les_Elements = Racine.GetFileSystemInfos();
+
+        foreach(var Item in Les_Elements) {
+
+            if((Item.Attributes & FileAttributes.Directory) == FileAttributes.Directory) {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Repertoires:");
+                Console.WriteLine($"Nom: {Item.Name}");
+                Console.WriteLine($"Emplacement: {Item.FullName}");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("--------------------");
+                Affiche_Info_Repertoire_V2(Item.FullName);
+            }
+
+            if((Item.Attributes & FileAttributes.Normal) == FileAttributes.Normal) {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Fichiers:");
+                Console.WriteLine($"Nom: {Item.Name}");
+                Console.WriteLine($"Emplacement: {Item.FullName}");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("========FIN_REPERTOIRE============");
+            }
+        }
+    }
+    static void Main(string[] args) {
+
+        string Chemin_Racine = "Y:\\Bts-Sio\\C#\\DEMO_REPERTOIRE";
+        Affiche_Info_Repertoire_V2(Chemin_Racine);
+
+    }
+}

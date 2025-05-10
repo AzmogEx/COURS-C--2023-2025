5b70f3f07fb8189997a27abbadd9370e0c0a9932
Adam M <80716388+AzmogEx@users.noreply.github.com>
2025-05-10 20:01:11 +0200

    Upload files from devops

---
+++ DEMO_LISTBOX/DEMO_LISTBOX/DATA/PERSONNE.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEMO_LISTBOX.DATA;
+public class PERSONNE {
+
+    public string Nom { get; set; }
+    public string Prenom { get; set; }
+
+    public override string ToString() {
+        return $"{Nom} - {Prenom}";
+    }
+}
+public class BASE {
+    List<PERSONNE> Les_personnes = null;
+    public BASE() {
+        Les_personnes = new List<PERSONNE>();
+        for(int Compteur = 0; Compteur < 10; Compteur++) {
+            PERSONNE Nouvelle = new PERSONNE() {
+                Nom = $"NOM_{Compteur + 1}",
+                Prenom = $"Prenom_{Compteur + 1}"
+            };
+
+            Les_personnes.Add(Nouvelle);
+        }
+    }
+    public List<PERSONNE> Get_All_Personnes() {
+        return Les_personnes;
+    }
+}
---
+++ DEMO_LISTBOX/DEMO_LISTBOX/VUES/CADRE.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DEMO_LISTBOX.DATA;
+using System.Windows;
+
+namespace DEMO_LISTBOX; 
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class CADRE :Window {
+
+    BASE La_Base = null;
+    
+    public CADRE() {
+
+        La_Base = new BASE();
+        InitializeComponent();
+
+        //List<int> Les_entiers = new List<int>();
+        //for(int Compteur = 0; Compteur < 10; Compteur++) {
+        //    LST_Info.Items.Add(Compteur);
+        //}
+        //LST_Info.ItemsSource = Les_entiers;
+
+        LST_Info.ItemsSource = La_Base.Get_All_Personnes();
+
+
+    }
+    PERSONNE Personne_Selectionnee = null;
+    private void LST_Info_SelectionChanged(object sender,System.Windows.Controls.SelectionChangedEventArgs e) {
+        Personne_Selectionnee = (PERSONNE)LST_Info.SelectedItem;
+
+        if (Personne_Selectionnee != null) {
+            TXT_Nom.Text = Personne_Selectionnee.Nom;
+            TXT_Prenom.Text = Personne_Selectionnee.Prenom;
+        }
+    }
+}
